7784be337ef04348d7e22d589bb9649daa8581e5
Vinícius <99450704+venysssssssssss@users.noreply.github.com>
2026-02-09 11:49:07 -0300

    Delete Base_mes_Efetividade.py

--- Base_mes_Efetividade.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), "src"))
-
-from extract_enel_sftp.processing.ordens_filhas import processar_ordens_filhas
-
-__all__ = ["processar_ordens_filhas"]
